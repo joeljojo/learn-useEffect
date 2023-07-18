e7f54cfe0a5defc6f54c4d1b2f4e5f9d1243e2d1
joeljojo <joelpamphyl@gmail.com>
2023-07-18 18:33:21 +0300

    Implemented useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+
+    // fetch data from API
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data = await response.json();
+
+        if (isMounted) {
+          setProducts(data.products);
+        }
+      } catch (error) {
+        console.log("Sever error, try again later");
+      }
+    };
+
+    // call the function
+    fetchProducts();
+    // Add clean up
+    return () => {
+      isMounted = false;
+    };
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="product-list">
+        {products.map((product) => (
+          <div className="product-card" key={product.id}>
+            <div className="product-image">
+              <img src={product.thumbnail} alt={product.title} />
+            </div>
+            <h3 className="product-title">{product.title}</h3>
+            <p className="product-description">{product.description}</p>
+            <p className="product-price">&#36; {product.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
